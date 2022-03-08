edff061440a8bc16accd261507ffdfc41f54c315
ShinKaruma <77399452+ShinKaruma@users.noreply.github.com>
2022-03-08 13:40:09 +0100

    commencement de la connexion

--- SEJOURBBP/app/src/main/java/com/example/sejourbbp/JsonPlaceHolderApi.java
+++ SEJOURBBP/app/src/main/java/com/example/sejourbbp/JsonPlaceHolderApi.java
@@ -5,10 +5,16 @@ import com.example.sejourbbp.ui.Patient;
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface JsonPlaceHolderApi {
 
     @GET("api/patients")
     Call<List<Patient>> getPatients();
+
+
+    @POST("/api/login_check")
+    Call<String> getToken(@Body String username, String password);
 }
--- SEJOURBBP/app/src/main/java/com/example/sejourbbp/LoginActivity.java
+++ SEJOURBBP/app/src/main/java/com/example/sejourbbp/LoginActivity.java
@@ -3,6 +3,17 @@ package com.example.sejourbbp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.sejourbbp.ui.Patient;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.POST;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +21,38 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("192.168.1.24/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        EditText InUsername = findViewById(R.id.InUsername);
+        EditText InPassword = findViewById(R.id.InPassword);
+        TextView output = findViewById(R.id.outputlogin);
+
+
+        Button btnConnexion = findViewById(R.id.btnConnexion);
+        btnConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = InUsername.getText().toString();
+                String password = InPassword.getText().toString();
+
+                Call<String> call = jsonPlaceHolderApi.getToken(username, password);
+
+                call.enqueue(
+                @Override
+                    public void onResponse(Call<String> call, Response<String> response) {
+                        if(!response.isSuccessful()){
+                            listpatient.setText("Code: "+response.code());
+                            return;
+                        }
+                    }
+
+                );
+            }
+        });
     }
 }
