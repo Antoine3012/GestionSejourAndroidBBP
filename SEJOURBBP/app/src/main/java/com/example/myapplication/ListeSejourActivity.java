895b8aaf868a9c99c6d5b8f0bafcf3fe11c5dd61
Antoine3012 <77623896+Antoine3012@users.noreply.github.com>
2022-04-01 09:55:24 +0200

    Fonctionnalité Séjours

--- SEJOURBBP/app/src/main/java/com/example/myapplication/ListeSejourActivity.java
+++ SEJOURBBP/app/src/main/java/com/example/myapplication/ListeSejourActivity.java
@@ -1,19 +1,24 @@
 package com.example.myapplication;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.icu.util.Calendar;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
-
-import com.example.myapplication.ui.login.LoginActivity;
 
 import java.util.List;
 
@@ -35,33 +40,58 @@ public class ListeSejourActivity extends AppCompatActivity {
     private ArrayAdapter<Sejour> listViewAdapter;
 
     private String token= "Bearer ";
+    private String context;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_liste_sejour);
 
         token += this.getIntent().getStringExtra("token");
+        context = this.getIntent().getStringExtra("context");
 
         ListView lstSejour = findViewById(R.id.lstviewSejour);
+
+        //Récupération date du jour
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        String formattedDate = df.format(c.getTime());
 
         Call<List<Sejour>> call = apiAsker.getSejours("application/json", token);
         call.enqueue(new Callback<List<Sejour>>() {
             @Override
             public void onResponse(Call<List<Sejour>> call, Response<List<Sejour>> response) {
 
+                //En cours
                 if(!response.isSuccessful()){
-                    Log.e("token", token);
-//                    A voir pourquoi ce n'est pas successful
                     Log.e("Code ", String.valueOf(response.code()));
                     Log.e("message", response.message());
                     return;
                 }else {
                 sejourList = response.body();
+                ArrayList<Sejour> lesSejourEnCours = new ArrayList<>();
+                for(Sejour unSejour:sejourList){
+                    try {
+                        java.util.Date date = df.parse(formattedDate);
+                        if(unSejour.getDateArrivee().compareTo(date)<=0){
+                            lesSejourEnCours.add(unSejour);
+                        }
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                }
 
-                listViewAdapter = new ArrayAdapter<Sejour>(ListeSejourActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, sejourList);
+                listViewAdapter = new ArrayAdapter<Sejour>(ListeSejourActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, lesSejourEnCours);
 
                 lstSejour.setAdapter(listViewAdapter);
+
+                //A venir
+                //Ce jour
+                //DatePrecise
+                //Patients
+                //Archives
+
                 }
             }
 
--- SEJOURBBP/app/src/main/java/com/example/myapplication/SejourActivity.java
+++ SEJOURBBP/app/src/main/java/com/example/myapplication/SejourActivity.java
@@ -39,72 +39,74 @@ public class SejourActivity extends AppCompatActivity {
 
         //Bouton En cours
         Button btnEnCours = findViewById(R.id.btnEnCours);
+        String encours = "encours";
         btnEnCours.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SejourActivity.this, ListeSejourActivity.class);
                 intent.putExtra("token", token);
                 intent.putExtra("login", login);
+                intent.putExtra("context", encours);
                 startActivity(intent);
             }
         });
 
         //Bouton A Venir
         Button btnAVenir = findViewById(R.id.btnAVenir);
-        Intent intent2 = new Intent(SejourActivity.this, ListeSejourActivity.class);
         btnAVenir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent2 = new Intent(SejourActivity.this, ListeSejourActivity.class);
                 startActivity(intent2);
             }
         });
 
         //Bouton DebutantCeJour
         Button btnDebutantCeJour = findViewById(R.id.btnDebutantCeJour);
-        Intent intent3 = new Intent(SejourActivity.this, ListeSejourActivity.class);
         btnDebutantCeJour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent3 = new Intent(SejourActivity.this, ListeSejourActivity.class);
                 startActivity(intent3);
             }
         });
 
         //Bouton DatePrecise
         Button btnDatePrecise = findViewById(R.id.btnDatePrecise);
-        Intent intent4 = new Intent(SejourActivity.this, SejourDateActivity.class);
         btnDatePrecise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent4 = new Intent(SejourActivity.this, SejourDateActivity.class);
                 startActivity(intent4);
             }
         });
 
         //Bouton Patients
         Button btnSejourPatient = findViewById(R.id.btnSejourPatient);
-        Intent intent5 = new Intent(SejourActivity.this, SejourPatientActivity.class);
         btnSejourPatient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent5 = new Intent(SejourActivity.this, SejourPatientActivity.class);
                 startActivity(intent5);
             }
         });
 
         //Bouton Archives
         Button btnArchiveSejour = findViewById(R.id.btnArchiveSejour);
-        Intent intent6 = new Intent(SejourActivity.this, ListeSejourActivity.class);
         btnArchiveSejour.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent6 = new Intent(SejourActivity.this, ListeSejourActivity.class);
                 startActivity(intent6);
             }
         });
 
         //Bouton Liste Patient
         Button btnListePatient = findViewById(R.id.btnlstpatient);
-        Intent intent7 = new Intent(SejourActivity.this, PatientActivity.class);
         btnListePatient.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intent7 = new Intent(SejourActivity.this, PatientActivity.class);
                 intent7.putExtra("token", token);
                 startActivity(intent7);
             }
